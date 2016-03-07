8f6ed5f009885c124b184ba789eb68a75cee52fd
hewell <hewell@hewells-MacBook-Pro.local>
2016-03-08 05:27:19 +0800

    p49, post endpoint

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+var express    = require('express')
+var bodyParser = require('body-parser')
+var Post       = require('./models/post')
 
-var app = express();
-app.use(bodyParser.json());
+var app = express()
+app.use(bodyParser.json())
 
-app.get('/api/posts', function(req, res){
-    res.json([
-        {
-            username: 'aaa',
-            body: 'node sample'
-        }
-    ])
+app.get('/api/posts', function (req, res, next) {
+  Post.find(function (err, posts) {
+    if (err) { return next(err) }
+    res.json(posts)
+  })
 })
 
-app.post('/api/posts', function(req, res){
-    console.log('post received!');
-    console.log(req.body.username);
-    console.log(req.body.body);
-    res.send(201);
+app.post('/api/posts', function (req, res, next) {
+  var post = new Post({
+    username: req.body.username,
+    body:     req.body.body,
+  })
+  post.save(function (err, post) {
+    if (err) { return next(err) }
+    res.status(201).json(post)
+  })
 })
 
-app.listen(3000, function(){
-    console.log('Server listening on', 3000);
+app.listen(3000, function () {
+  console.log('server listening on', 3000)
 })
