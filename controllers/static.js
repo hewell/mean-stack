490681bde61c17408a6503043cec446028e2a8b0
hewell <hewell@gmail.com>
2016-03-08 23:42:48 +0800

    p64, serving static files from node

--- controllers/static.js
+++ controllers/static.js
@@ -1,7 +1,11 @@
-var router = require('express').Router();
+var express = require('express');
+var router = express.Router();
+
+router.use('/static', express.static(__dirname + '/../assets'));
+router.use('/bower_static', express.static(__dirname + '/../bower_components'));
 
 router.get('/', function (req, res) {
-    res.sendFile('layouts/posts.html', {root: '.'});
+    res.sendFile('layouts/posts.html', {root: __dirname + "/.."});
 });
 
 module.exports = router;
