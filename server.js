bd40f360b89eafabf32107305d63fcc4d5586310
hewell <hewell@gmail.com>
2016-03-08 18:11:31 +0800

    code formatted using js-beautifier

--- server.js
+++ server.js
@@ -1,35 +1,41 @@
-var express    = require('express')
+var express = require('express')
 var bodyParser = require('body-parser')
-var Post       = require('./models/post')
+var Post = require('./models/post')
 var root_dir = '.';
 
 var app = express()
 app.use(bodyParser.json())
 
-app.get('/', function(req, res) {
-	res.sendFile('layouts/posts.html', { root: root_dir });
+app.get('/', function (req, res) {
+    res.sendFile('layouts/posts.html', {
+        root: root_dir
+    });
 })
 
 app.get('/api/posts', function (req, res, next) {
-  Post.find()
-    .sort('-date')
-    .exec(function (err, posts) {
-    if (err) { return next(err) }
-    res.json(posts)
-  })
+    Post.find()
+        .sort('-date')
+        .exec(function (err, posts) {
+            if (err) {
+                return next(err)
+            }
+            res.json(posts)
+        })
 })
 
 app.post('/api/posts', function (req, res, next) {
-  var post = new Post({
-    username: req.body.username,
-    body:     req.body.body,
-  })
-  post.save(function (err, post) {
-    if (err) { return next(err) }
-    res.status(201).json(post)
-  })
+    var post = new Post({
+        username: req.body.username,
+        body: req.body.body,
+    })
+    post.save(function (err, post) {
+        if (err) {
+            return next(err)
+        }
+        res.status(201).json(post)
+    })
 })
 
 app.listen(3000, function () {
-  console.log('server listening on', 3000)
+    console.log('server listening on', 3000)
 })
